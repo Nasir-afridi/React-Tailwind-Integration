4473e7fe315aa78038a1ae0e75298c83abb03d69
Nasir-afridi <nasirwc05@gmail.com>
2024-09-17 13:23:08 +0500

    Intergration of tailwind with react

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="italic	font-style: italic; text-xl font-extrabold text-rose-500">
+        Integration of Tailwind in React js
+      </h1>
+    </>
+  );
+}
+
+export default App;
